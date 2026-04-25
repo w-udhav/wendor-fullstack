079f6734dbc23bebfabefabf0f11f7543657babf
w-udhav <udhavwadhawan@hotmail.com>
2026-04-25 06:28:07 +0530

    updated constants

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,5 +1,5 @@
-export const baseURL = "http://localhost:3000/api";
-// export const baseURL = "https://wendor-server.prathammehta.xyz/api";
+// export const baseURL = "http://localhost:3000/api";
+export const baseURL = "https://kiosk-server-q00y.onrender.com/api";
 
 export const categories = [
   "Beverages",
